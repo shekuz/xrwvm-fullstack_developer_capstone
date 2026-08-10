9b056fadc749bc71ee592dab77fe251f2eff31a7
Shekuz <shekuprog@gmail.com>
2026-08-10 11:13:39 -0400

    Add a new Login View

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,16 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=['localhost','https://github.com/shekuz/xrwvm-fullstack_developer_capstone.git']
-CSRF_TRUSTED_ORIGINS=['https://github.com/shekuz/xrwvm-fullstack_developer_capstone.git']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'shekuprog-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://shekuprog-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+]
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -61,8 +69,10 @@ ROOT_URLCONF = 'djangoproj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-                'DIRS': [
-            os.path.join(BASE_DIR,'frontend/static')
+                        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend/static'),
+            os.path.join(BASE_DIR, 'frontend/build'),
+            os.path.join(BASE_DIR, 'frontend/build/static'),
         ],
 
         'APP_DIRS': True,
@@ -138,7 +148,10 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'frontend/static')
+    os.path.join(BASE_DIR, 'frontend/static'),
+	os.path.join(BASE_DIR, 'frontend/build'),
+	os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
+
 
 
